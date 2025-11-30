b4446b2304b376fabeb76c1d1917cef983e22a7d
YogeshCheedalla <cheedallayogesh05@gmail.com>
2025-11-30 07:30:25 +0530

    fix Student.jsx css import

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Student.css";
+import "./student.css";
+
 import { projects } from "./projects";
 
 const Student = () => {
